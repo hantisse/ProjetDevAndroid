674d265f1c52a412f25298291671d15c9b41d298
LittleFlameMan <marc.chicourrat-poublan@telecom-sudparis.eu>
2018-11-30 01:25:52 +0100

    Ajout du BitMapDownloader pour l'écran de détails des cartes

---
+++ app/src/main/java/com/judith/h/projetdevandroid/editor/AsyncBitmapDownloader.java
@@ -0,0 +1,59 @@
+package com.judith.h.projetdevandroid.editor;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.judith.h.projetdevandroid.Card;
+import com.judith.h.projetdevandroid.DecksDataBaseHelper;
+import com.judith.h.projetdevandroid.R;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AsyncBitmapDownloader extends AsyncTask<String, Void, Bitmap> {
+
+    CardActivity cardActivity;
+
+    AsyncBitmapDownloader(CardActivity cardActivity){
+        this.cardActivity = cardActivity;
+    }
+
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        String id = strings[0];
+        DecksDataBaseHelper dataBaseHelper = new DecksDataBaseHelper();
+        Card card = dataBaseHelper.getCard(Long.parseLong(id) );
+        URL url = null;
+        HttpURLConnection urlConnection = null;
+        Bitmap bm = null;
+        try {
+            url = new URL(strings[0]);
+            Log.i("CIO", "Downloading " + url);
+            urlConnection = (HttpURLConnection) url.openConnection(); // Open
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream()); // Stream
+            bm = BitmapFactory.decodeStream(in);
+            in.close();
+        }
+        catch (MalformedURLException e) { e.printStackTrace(); }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (urlConnection != null)
+                urlConnection.disconnect();
+        }
+        return bm;
+    }
+
+    protected void onPostExecute(Bitmap bitmap){
+        ImageView image = cardActivity.findViewById(R.id.CardImage);
+        image.setImageBitmap(bitmap);
+    }
+}
