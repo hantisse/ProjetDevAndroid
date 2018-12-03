9d543c4a40dec8fd71e9478b8d2c549e073b327b
LittleFlameMan <marc.chicourrat-poublan@telecom-sudparis.eu>
2018-12-03 23:41:46 +0100

    Suppression de decks, ajout de menus + et - pour la visualisation de carte

--- app/src/main/java/com/judith/h/projetdevandroid/editor/CardActivity.java
+++ app/src/main/java/com/judith/h/projetdevandroid/editor/CardActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.judith.h.projetdevandroid.Card;
@@ -30,6 +32,30 @@ public class CardActivity extends Activity {
         String name = card.getName();
         TextView title = findViewById(R.id.cardtitle);
         title.setText(name);
+
+        final TextView cardmultiplicity = findViewById(R.id.cardmultiplicity);
+        cardmultiplicity.setText("0");
+
+        Button lesscard = findViewById(R.id.lesscard);
+        lesscard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int multiplicity = Integer.parseInt(cardmultiplicity.getText().toString());
+                if (multiplicity != 0){
+                    multiplicity = multiplicity - 1;
+                }
+                cardmultiplicity.setText(Integer.toString(multiplicity));
+            }
+        });
+
+        Button morecard = findViewById(R.id.morecard);
+        morecard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int multiplicity = Integer.parseInt(cardmultiplicity.getText().toString()) + 1;
+                cardmultiplicity.setText(Integer.toString(multiplicity));
+            }
+        });
 
         String url = card.getImgUrl();
 
